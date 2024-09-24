b8bb68c9e79340e54b72a0c7f5f3e95f1b37e500
albert <dreak603@gmail.com>
2024-09-24 17:33:09 +0800

    build: add makefile and include server in compose file

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -7,11 +7,11 @@ const config = {
   name: 'db',
   connector: 'mysql',
   url: '',
-  host: 'localhost',
-  port: 3306,
-  user: 'user',
-  password: 'userpassword',
-  database: 'lb4',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  user: process.env.DB_USER || 'user',
+  password: process.env.DB_PASSWORD || 'userpassword',
+  database: process.env.DB_DATABASE || 'lb4',
 };
 
 @lifeCycleObserver('datasource')
